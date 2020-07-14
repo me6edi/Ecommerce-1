ba90671a4d1f6a468cbe6ebe31d2e7ce4e4ab034
me6edi <exited17@gmail.com>
2020-07-14 21:07:03 +0600

    complite ecomomerce web-site

--- store/urls.py
+++ store/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
 	 
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
-
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse 
 import json
+import datetime
 
 from .models import * 
  
@@ -35,7 +36,7 @@ def cart(request):
  
     context = {'items':items,'order':order, 'cartItems':cartItems}
     return render (request, 'store/cart.html' , context)
- 
+
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
@@ -77,5 +78,33 @@ def updateItem(request):
 
     return JsonResponse('Item was added', safe=False)
 
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
 def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float (data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_total):
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            shippingAddress.objects.create(   
+                customer = customer,
+                order = order,
+                address = data['shipping']['address'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+
+            )
+
+    else:
+        print('User is not logged in..')
     return JsonResponse('Payment complete!', safe=False)
